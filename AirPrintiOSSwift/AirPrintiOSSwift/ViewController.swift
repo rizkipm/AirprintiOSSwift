4b1b5b5c23fb54e33e1e88192b2d5c1f412387ea
Programmer Muslim <rizki@imastudio.co.id>
2018-04-19 00:06:42 +0700

    add

--- AirPrintiOSSwift/AirPrintiOSSwift/ViewController.swift
+++ AirPrintiOSSwift/AirPrintiOSSwift/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtText: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func btnPrint(_ sender: Any) {
+        
+        // 1
+        let printController = UIPrintInteractionController.shared
+        // 2
+        let printInfo = UIPrintInfo(dictionary:nil)
+        printInfo.outputType = UIPrintInfoOutputType.general
+        printInfo.jobName = "print Job"
+        printController.printInfo = printInfo
+        
+        // 3
+        let formatter = UIMarkupTextPrintFormatter(markupText: txtText.text)
+        formatter.perPageContentInsets = UIEdgeInsets(top: 72, left: 72, bottom: 72, right: 72)
+        printController.printFormatter = formatter
+        
+        // 4
+        printController.present(animated: true, completionHandler: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
